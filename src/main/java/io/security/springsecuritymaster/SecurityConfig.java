de0f7b95cd8be8313d03b289db711c7f99f79b4e
Jiwoo <ojing0116@skuniv.ac.kr>
2026-03-19 14:38:38 +0900

    :sparkles: RememberMeAuthentication Filter

--- src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
         .formLogin(Customizer.withDefaults())
         .rememberMe(rememberMe -> rememberMe
-            //.alwaysRemember(true) //기억하기 매개변수가 설정 안됐을때도 쿠키가 항상 생성되어야하는지에 대한 여부
+            .alwaysRemember(true) //기억하기 매개변수가 설정 안됐을때도 쿠키가 항상 생성되어야하는지에 대한 여부
             .tokenValiditySeconds(3600)   //토큰이 유효한 시간(초 단위)를 지정할 수 있음
             .userDetailsService(userDetailsService())  //UserDetails조회를 위한 서비스 지정
             .rememberMeParameter("remember")  //로그인시 사용자 기억을 위한 HTTP 매개변수. 기본값 remember-me
